e4108720250b328c5554a149d21244591f7711ae
Oliver Vuong <vuongoliver@gmail.com>
2025-02-02 00:42:06 -0800

    Create util folder with definitions and utility functions

---
+++ src/utils/utils.js
@@ -0,0 +1,32 @@
+// set order in which roles will be displayed
+export const roles = [
+  'T1',
+  'T2',
+  'SCH',
+  'SGE',
+  'WHM',
+  'AST',
+  'M1',
+  'M2',
+  'PRange',
+  'Caster',
+];
+
+export const tankCombos = [
+  'WARGNB_WAR',
+  'WARGNB_GNB',
+  'WARPLD_WAR',
+  'WARPLD_PLD',
+  'WARDRK_WAR',
+  'WARDRK_DRK',
+  'GNBDRK_GNB',
+  'GNBDRK_DRK',
+  'GNBPLD_GNB',
+  'GNBPLD_PLD',
+  'PLDDRK_PLD',
+  'PLDDRK_DRK',
+];
+
+export const isTank = (input) => {
+  return input in tankCombos;
+};
