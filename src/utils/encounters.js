a3c46432672b5c70d21c9b7593bef10f6e391884
Oliver Vuong <vuongoliver@gmail.com>
2025-02-06 01:21:34 -0800

    Add partial DSR data

--- src/utils/encounters.js
+++ src/utils/encounters.js
@@ -1,10 +1,21 @@
 import data from '../data';
 
+// Futures Rewritten
 const FRU_fmbg = [data.fru.fmbg.p1, data.fru.fmbg.p2];
 const FRU_naur = [data.fru.naur.p1];
 
-const FRU = [FRU_fmbg, FRU_naur];
+const FRU = {
+  fmbg: FRU_fmbg,
+  naur: FRU_naur,
+};
 
-const encounters = [FRU];
+// Dragon Song Reprise
+const DSR_naur = [data.dsr.naur.p2, data.dsr.naur.p3];
+
+const DSR = {
+  naur: DSR_naur,
+};
+
+const encounters = { FRU, DSR };
 
 export default encounters;
