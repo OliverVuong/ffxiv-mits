d0a4c04d142b1024a9cfe52736100fdfb7d45937
Oliver Vuong <vuongoliver@gmail.com>
2025-02-12 18:55:30 -0800

    Add naur fru data

--- src/utils/encounters.js
+++ src/utils/encounters.js
@@ -5,6 +5,10 @@ import fru_fmbg_p4 from '../data/fru/fmbg/p4.json';
 import fru_fmbg_p5 from '../data/fru/fmbg/p5.json';
 
 import fru_naur_p1 from '../data/fru/naur/p1.json';
+import fru_naur_p2 from '../data/fru/naur/p2.json';
+import fru_naur_p3 from '../data/fru/naur/p3.json';
+import fru_naur_p4 from '../data/fru/naur/p4.json';
+import fru_naur_p5 from '../data/fru/naur/p5.json';
 
 import dsr_naur_p2 from '../data/dsr/naur/p2.json';
 import dsr_naur_p3 from '../data/dsr/naur/p3.json';
@@ -18,7 +22,13 @@ const FRU_fmbg = [
   fru_fmbg_p5,
 ];
 
-const FRU_naur = [fru_naur_p1];
+const FRU_naur = [
+  fru_naur_p1,
+  fru_naur_p2,
+  fru_naur_p3,
+  fru_naur_p4,
+  fru_naur_p5,
+];
 
 const FRU = {
   fmbg: FRU_fmbg,
