1620c35e970e42d579edc1d1781cdc1aba660c58
Oliver Vuong <vuongoliver@gmail.com>
2025-02-13 01:15:39 -0800

    Flex tank menu selection according to mit plan selected

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -25,7 +25,38 @@ export const tankCombos = [
   'GNBPLD_PLD',
   'PLDDRK_PLD',
   'PLDDRK_DRK',
+  'PLDGNB_PLD',
+  'PLDGNB_GNB',
+  't1',
+  't2',
 ];
+
+export const tankStateDefault = {};
+for (const combo of tankCombos) {
+  tankStateDefault[combo] = false;
+}
+
+export const tankCombosByPlan = {
+  FRU: {
+    fmbg: [
+      ['WARGNB_WAR', 'WARGNB_GNB'],
+      ['WARPLD_WAR', 'WARPLD_PLD'],
+      ['WARDRK_WAR', 'WARDRK_DRK'],
+      ['GNBDRK_GNB', 'GNBDRK_DRK'],
+      ['GNBPLD_GNB', 'GNBPLD_PLD'],
+      ['PLDDRK_PLD', 'PLDDRK_DRK'],
+    ],
+    naur: [
+      ['WARGNB_WAR', 'WARGNB_GNB'],
+      ['WARPLD_WAR', 'WARPLD_PLD'],
+      ['WARDRK_WAR', 'WARDRK_DRK'],
+      ['GNBDRK_GNB', 'GNBDRK_DRK'],
+      ['PLDGNB_PLD', 'PLDGNB_GNB'],
+      ['PLDDRK_PLD', 'PLDDRK_DRK'],
+    ],
+    frumitty: [['t1', 't2']],
+  },
+};
 
 export const isTank = (input) => {
   return tankCombos.includes(input);
