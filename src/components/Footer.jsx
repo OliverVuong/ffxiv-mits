d9f9c2acc0e8d0cfe40678e177b0dff0fdbfd065
Oliver Vuong <vuongoliver@gmail.com>
2025-07-26 01:06:25 -0700

    Change footer wording

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(){
             <hr className='footer-divider'/>
             <div className='footer-content'>
                 <div>FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd. FINAL FANTASY XIV © SQUARE ENIX</div>
-                <div className='right'>Made by Jaye Money-Ultros</div>
+                <div className='right'>Created by Jaye Money@Ultros</div>
     
             </div>
             </footer>
