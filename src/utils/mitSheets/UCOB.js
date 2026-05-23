6e9ba9dd8395f5d3f91eaa130a3d82260844661b
Oliver Vuong <vuongoliver@gmail.com>
2026-05-23 00:22:24 -0700

    Add UCOB json and utils

---
+++ src/utils/mitSheets/UCOB.js
@@ -0,0 +1,14 @@
+import ucob_p1 from '../../data/ucob/p1.json';
+import ucob_p2 from '../../data/ucob/p2.json';
+import ucob_p3 from '../../data/ucob/p3.json';
+import ucob_p4 from '../../data/ucob/p4.json';
+import ucob_p5 from '../../data/ucob/p5.json';
+
+const UCOB = {
+  data: [ucob_p1, ucob_p2, ucob_p3, ucob_p4, ucob_p5],
+  tankCombos: [['MT', 'OT']],
+  extras: ['BRD', 'MCH', 'PLD', 'MNK'],
+  name: `The Unending Coil of Bahamut`,
+};
+
+export default UCOB;
