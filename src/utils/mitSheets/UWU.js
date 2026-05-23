53965fe170ef6121accee014af9a7bb2221b9589
Oliver Vuong <vuongoliver@gmail.com>
2026-05-23 01:19:27 -0700

    Add UWU json data and util

---
+++ src/utils/mitSheets/UWU.js
@@ -0,0 +1,14 @@
+import uwu_p1 from '../../data/uwu/p1.json';
+import uwu_p2 from '../../data/uwu/p2.json';
+import uwu_p3 from '../../data/uwu/p3.json';
+import uwu_p4 from '../../data/uwu/p4.json';
+import uwu_p5 from '../../data/uwu/p5.json';
+
+const UWU = {
+  data: [uwu_p1, uwu_p2, uwu_p3, uwu_p4, uwu_p5],
+  tankCombos: [['MT', 'OT']],
+  extras: ['BRD', 'MCH', 'PLD', 'MNK'],
+  name: `The Weapon's Refrain`,
+};
+
+export default UWU;
